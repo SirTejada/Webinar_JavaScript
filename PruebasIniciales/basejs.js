b028792fc712f8c28ebe77f7587686def255bb65
Alejandro <alejandro.tejada@soprasteria.com>
2024-06-03 10:51:48 +0200

    Primera Funcion

--- PruebasIniciales/basejs.js
+++ PruebasIniciales/basejs.js
@@ -4,13 +4,17 @@
 // var
 // const 
 
-let numero1 = 2;
+let numero1 = "2";
 let numero2 = 2;
 const _YEAR = "2024";
 
-let numero3 = numero1 + numero2;
 
 
-console.log(numero3 + " " + _YEAR);
+let numero3 = Number(numero1) + numero2;
 
+console.log(getNumber() + " " + _YEAR);
 
+function getNumber(){
+    let numero = Number(numero1) + numero2;
+    return numero; 
+}
