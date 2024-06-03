674b9796bb85fd46ca517acc651592e5317e7556
Alejandro <alejandro.tejada@soprasteria.com>
2024-06-03 10:32:43 +0200

    Tipos de variables de JavaScript

--- PruebasIniciales/basejs.js
+++ PruebasIniciales/basejs.js
@@ -1,3 +1,16 @@
-alert("Manolo branch alejandro");
-alert("Segunda alerta");
-// prueba desde alejandro una vez merged
+//alert("Manolo branch alejandro");
+
+// let
+// var
+// const 
+
+let numero1 = 2;
+let numero2 = 2;
+const _YEAR = "2024";
+
+let numero3 = numero1 + numero2;
+
+
+console.log(numero3 + " " + _YEAR);
+
+
